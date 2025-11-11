59ebbb08031326719d6041194ab1235ca2f1c144
Iwanezuk <iwanezuk@hotmail.com>
2025-11-10 23:09:56 -0300

    modified:   petshop/src/components/cabecalho.jsx

--- petshop/src/components/cabecalho.jsx
+++ petshop/src/components/cabecalho.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../assets/css/components/cabecalho.css';
 import imagem from '../assets/img/catiorrinho.svg';
 
@@ -8,18 +9,18 @@ const Cabecalho = () => {
             <span className="menu-hamburguinho__icone"></span>
         </div>
         <div className="cabecalho-container">
-            <a href="/" className="flex flex--centro">
+            <Link to="/" className="flex flex--centro">
             <img className="cabecalho__logo" src={imagem} alt="Logo Catiorrinho"/>
-            <h1 className="cabecalho__titulo">PetShop</h1>
-            </a>
+            <h1 className="cabecalho__titulo">Petshop</h1>
+            </Link>
         </div>
 
         <nav className="menu-cabecalho">
             <ul className="menu-itens">
-            <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-            <li><a href="#" className="menu-item">Produtos</a></li>
-            <li><a href="/" className="menu-item">Blog</a></li>
-            <li><a href="/sobre" className="menu-item">Sobre</a></li>
+                <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                <li><Link to="#" className="menu-item">Produtos</Link></li>
+                <li><Link to="/" className="menu-item">Blog</Link></li>
+                <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
             </ul>
         </nav>
         <div className="menu-cabecalho-background"></div>
