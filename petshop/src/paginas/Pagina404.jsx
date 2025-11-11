3cf5af9fca353fb31de706d9512f34c19c7d157e
Iwanezuk <iwanezuk@hotmail.com>
2025-11-10 22:33:12 -0300

    Update Pagina404.jsx

--- petshop/src/paginas/Pagina404.jsx
+++ petshop/src/paginas/Pagina404.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import '../assets/css/404.css';
 import imagem from '../assets/img/catiorrinho404.svg'
 
 const Pagina404 = () => {
     return(
         <main className="container flex flex--centro flex--coluna">
-            <img className="catiorrinho-imagem" src="{imagem}" alt="Ilustração Catiorinho" />
+            <img className="catiorrinho-imagem" src={imagem} alt="Ilustração Catiorinho" />
             <p className="naoencontrado-texto">
                 Au, au, foi mau!
             </p>
